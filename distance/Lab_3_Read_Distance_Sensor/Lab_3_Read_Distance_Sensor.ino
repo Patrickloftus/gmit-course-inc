222fe3c3fa6d689a7bba06426bf9d461c8ea6676
Patrickloftus <patloftus01@gmail.com>
2021-01-30 22:11:48 +0000

    End of Lab 3

--- distance/Lab_3_Read_Distance_Sensor/Lab_3_Read_Distance_Sensor.ino
+++ distance/Lab_3_Read_Distance_Sensor/Lab_3_Read_Distance_Sensor.ino
@@ -19,18 +19,24 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
 
-   int sensorValue = analogRead(A0);     
-   float voltage = sensorValue * (5.0 / 1023.0);  
+   int sensorValue = analogRead(A0);              // we are reading in our signal from the distance sensor into analog pin 0
+   float voltage = sensorValue * (5.0 / 1023.0);  // we are converting our analog sensorvalue from ADC to voltage
     
-    //y = mx + c
+    // y = mx + c  // formula for slope of line
     
-   float distance = -22.58*voltage+58.591;  // we are doing this formula to calibrate our distance sensor
+   float distance = -22.883*voltage+62.143;  // we are doing this formula to calibrate our distance sensor
                                             // voltage is now been converted to a distance
                                             
    
    Serial.print("X,"); Serial.print(distance); Serial.print("  ");   
    Serial.println(" ");
    delay(200);
+   
   // note that if you change the serial print to sensorValue you can see the real value from the sensor
- // note also to add the delay and notice changes on input speed 
+  // note also to add the delay and notice changes on input speed 
+  // after calibration there was a much steadier reading on the serial plotter
+  // noticed how as you move the sensor closer to the material i was hitting a deadzone
+
+
+  // this was a interesting online lab, thanks stephen
 }
